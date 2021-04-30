efd976305fea16a9f31ffd92c222eb67d92d3b75
EC2 Default User <ec2-user@ip-172-31-0-41.ap-south-1.compute.internal>
2021-04-30 06:23:48 +0000

    s3

---
+++ s3/output.tf
@@ -0,0 +1,6 @@
+output "s3_bucket_arn" {
+  description = "The ARN of the bucket. Will be of format arn:aws:s3:::bucketname."
+  value       = "${aws_s3_bucket.bucket.arn}"
+
+}
+
---
+++ s3/s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
+  tags = {
+    Name        = var.bucket_tag_name
+    Environment = "Dev"
+  }
+}
+
+
