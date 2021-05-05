f50e7f2a41e6b673c901f7e53918b44dc29b5b44
Navneet Singh <76422094+navneetguraya@users.noreply.github.com>
2021-05-05 12:26:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.region
-  shared_credentials_file = "${var.project_name}.credential"
+  shared_credentials_file = "${var.project_name}.credentials"
   profile = var.environment_name
 }
